739bc45f4adfa6a3e2dee9490883e8316bae929e
Femboy hub <harleydebruyn2012@gmail.com>
2026-02-26 11:05:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log('Een gebruiker is verbonden');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Stuurt naar iedereen
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Een gebruiker is losgekoppeld');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server draait op http://localhost:${PORT}`);
+});
